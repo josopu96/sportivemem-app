31d73b7845809bc024572ee4a2ca6fe37b18d294
Jose Daniel <jossolpue@alum.us.es>
2021-10-09 11:26:48 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'sportivemem-app',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
